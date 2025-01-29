e437dab9fc35b5ba858967b87b936ab6b5a7f827
lohochris <159076124+lohochris@users.noreply.github.com>
2025-01-29 14:57:15 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
--- Use the information_schema to get column details of the Books table
+-- Retrieve the full description of the 'Books' table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.COLUMNS
+FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
