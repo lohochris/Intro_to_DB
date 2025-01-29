17587f250afdde8c65cd53fb762591719bd14388
lohochris <159076124+lohochris@users.noreply.github.com>
2025-01-29 13:42:35 +0100

    Add files via upload

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+-- Use the database alx_book_store
+USE alx_book_store;
+
+-- Show all tables in the database
+SHOW TABLES;
--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,8 @@
-INSERT INTO customer (customer_id, customer_name, email, address)
+-- Use the database alx_book_store
+USE alx_book_store;
+
+-- Insert multiple customers into the 'customers' table
+INSERT INTO customers (customer_id, name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
