acff2b3649817af9138c0dcae110f7a1aeecb709
lohochris <159076124+lohochris@users.noreply.github.com>
2025-01-29 14:05:15 +0100

    Delete task_6.sql

--- task_6.sql
+++
@@ -1,9 +0,0 @@
--- Use the database alx_book_store
-USE alx_book_store;
-
--- Insert multiple customers into the 'customers' table
-INSERT INTO customers (customer_id, name, email, address)
-VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
