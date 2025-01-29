a77f1e95f9e92a3b1c82f32360e8d9acddf44d03
lohochris <159076124+lohochris@users.noreply.github.com>
2025-01-29 14:05:39 +0100

    Delete task_4.sql

--- task_4.sql
+++
@@ -1,7 +0,0 @@
--- Use the database alx_book_store
-USE alx_book_store;
-
--- Show information about the 'Books' table
-SELECT * 
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
